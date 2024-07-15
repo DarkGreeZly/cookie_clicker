a08ee26eeb757d34b07c31bb705a8881035aa278
DarkGreeZly <lager7050@gmail.com>
2024-07-15 21:18:27 +0300

    feat | added frontend

---
+++ clicker_web/frontend/clicker_frontend/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <button class="px-8 md-">
+      <img class="object-scale-down max-h-full drop-shadow-md rounded-md m-auto" src='../assets/cookie.png'/>
+    </button>
+  </main>
+</template>
